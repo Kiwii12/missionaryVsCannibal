39978ce5397c23a41fa7b3d008959152874568a0
jandanderson <jason.a.anderson@mines.sdsmt.edu>
2015-04-23 17:48:58 -0600

    Update and rename mandw.cpp to mandc.cpp

--- mandc.cpp
+++ mandc.cpp
@@ -34,20 +34,23 @@
 	(setq *cannibals* cannibals)
 
 	;Solve missionaries and cannibals problem using DFS
-	(format t "This is where DFS code goes.")
-	(start-state)
-	(format t "After start-state call.")
-	(generate-successors (start-state))
+        (setf start-state '(0 0 0))
+        ;The thrid argument for start state is which side of the bank the canoe is on
+	;0 for left 1 for right
+	(setf (car start-state) missionaries (cadr start-state) cannibals 
+		(caddr start-state) 0)
+	(format t "~%After start-state call.~%")
+        (format t "Missionaries ~S~%" missionaries)
+        (format t "Cannibals ~S~%" cannibals)
+        (format t "Start-state ~S~%" start-state)
+	(generate-successors start-state)
 
 	;suppress printing NIL upon return to interpreter
 	(values)
 )
 
-;Define the start state
-(defun start-state () '(missionaries cannibals left))
-
 ;Have we reached the goal? (all missionaries and cannibals on the right bank)
-(defun goal-state () '(0 0 right))
+(setf goal-state '(0 0 1))
 
 ;generate-successors returns a list of successor to the current state.
 (defun generate-successors (state)
@@ -55,29 +58,48 @@
 	(let (m1 c1 b1 (miss (car state)) (cann (cadr state)) (boat (caddr state)) 
 		(succs nil))
 
+		(format t "Boat: ~S~%" boat)
+		(format t "Succs: ~S~%" succs)
+
 		;move 2 missionaries
-		(when (string-equal boat "left") (setq succs 
-			(cons (list (- miss 2) cann right) succs)))
+		(when (and (eql (caddr state) 0) (>= miss 2))
+			(setq succs (cons (list (- miss 2) cann 1) succs))
+		(format t "Moving 2 missionaries: ~S~%" succs))
 		
 		;move 2 cannibals
-		(when (string-equal boat "left") (setq succs 
-			(cons (list miss (- cann 2) right) succs)))
+		(when (and (eql boat 0) (>= cann 2))
+			(setq succs (cons (list miss (- cann 2) 1) succs))
+		(format t "Moving 2 cannibals: ~S~%" succs))
 
 		;move 1 of each
-		(when (string-equal boat "left") (setq succs 
-			(cons (list (decf miss) (decf cann) right) succs)))
+		(when (and (eql boat 0) (>= miss 1)(>= cann 1))
+			(setq succs (cons (list (decf miss) (decf cann) 1) succs))
+		(format t "Moving 1 of each: ~S~%" succs))
 
 		;move 1 missionary back
-		(when (string-equal boat "right") (setq succs 
-			(cons (list (decf miss) cann left) succs)))
+		(when (eql boat 1) 
+			(setq succs (cons (list (decf miss) cann 0) succs))
+		(format t "Moving 1 missionary back: ~S~%" succs))
 
 		;move 1 cannibal back
-		(when (string-equal boat "right") (setq succs 
-			(cons (list miss (decf cann) left) succs)))
+		(when (eql boat 1) 
+			(setq succs (cons (list miss (decf cann) 0) succs))
+		(format t "Moving 1 cannibals back: ~S~%" succs))
 
 		;return list of successors, without duplicates
 		(remove-duplicates succs :test #'equal)
-	)
+		(nreverse succs)
+		(format t "Reversed succs: ~S~%" succs)
+		(loop while (not (equalp goal-state (car succs))) do
+			(setf miss (car (car succs)))
+			(setf cann (car (cdr (car succs))))
+			(format t "Miss: ~S~%" miss)
+			(format t "Cann: ~S~%" cann)
+			(if (< miss cann) 
+				(pop succs)
+				(generate-successors (car succs)))
+		)
+	)	
 )
 ;Run WaterJug automatically upon loading file
 (main)
